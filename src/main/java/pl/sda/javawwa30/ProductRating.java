3e1ecdd7b4f76f0fb3275ea7d3525bc4142e8ada
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-03-01 10:36:27 +0100

    Configured Maven to support JUnit4. Provided initial test example.

---
+++ src/main/java/pl/sda/javawwa30/ProductRating.java
@@ -0,0 +1,19 @@
+package pl.sda.javawwa30;
+
+public class ProductRating {
+
+    int score;
+
+    //odkomentuj zeby test 'create' nie udal sie
+    /*ProductRating() {
+        score = 1/0;
+    }*/
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
---
+++ src/test/java/pl/sda/javawwa30/ProductRatingTest.java
@@ -0,0 +1,12 @@
+package pl.sda.javawwa30;
+
+import org.junit.Test;
+
+public class ProductRatingTest {
+
+    @Test
+    public void create() {
+        ProductRating pr = new ProductRating();
+    }
+
+}
